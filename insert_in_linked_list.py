ce731adfe5845c74b5be5631d8473e3fca0b83be
shayanadc <shayanadc@gmail.com>
2019-12-17 12:08:32 +0330

    insert in linked list

---
+++ insert_in_linked_list.py
@@ -0,0 +1,49 @@
+from unittest import TestCase
+
+class testLinkedList(TestCase):
+
+    def test_traverse_linked_list(self):
+        llist = LinkedList()
+
+        llist.head = Node(1)
+        second = Node(2)
+        third = Node(3)
+        forth = Node(4)
+
+        llist.head.next = second
+        second.next = third
+        third.next = forth
+
+        llist.push(5)
+        self.assertEqual([5,1,2,3,4], llist.traverseList())
+
+
+class Node:
+
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+class LinkedList:
+
+    def __init__(self):
+        self.head = None
+
+    def traverseList(self):
+        arrList = []
+        temp = self.head
+
+        while (temp):
+            arrList.append(temp.data)
+            temp = temp.next
+
+        return arrList
+
+    def push(self, new_data):
+
+        new_node = Node(new_data)
+
+        new_node.next = self.head
+
+        self.head = new_node
